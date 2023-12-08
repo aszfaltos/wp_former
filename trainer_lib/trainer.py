198a343f9cbaff770ee5a996ed0a631ca1774a5e
aszfaltos <rnngqd@inf.elte.hu>
2023-12-08 15:04:13 +0100

    early stopping

--- trainer_lib/trainer.py
+++ trainer_lib/trainer.py
@@ -1,9 +1,10 @@
+import math
 import os.path
-
 from torch.utils.data import DataLoader
 from torch import nn, optim
 import torch
-from .utils import checkpoint, resume
+from .utils import checkpoint
+from .early_stop import EarlyStopper
 
 
 class Trainer:
@@ -15,14 +16,15 @@ class Trainer:
         self.batch_size = batch_size
         self.model = model
         self.epochs = epochs
-        self.metrics = {'train_loss': [], 'eval_loss': []}
+        self.metrics = {'train': {'MSE': []}, 'eval': {'MSE': [], 'RMSE': [], 'MAE': []}}
         self.save_path = save_path
         os.makedirs(self.save_path, exist_ok=True)
 
     def train(self, train_data, valid_data):
+        early_stopper = EarlyStopper(10, 0.1)
         optimizer = optim.Adam(self.model.parameters(), lr=1e-3, betas=(0.9, 0.98), eps=1e-9)
 
-        warmup = optim.lr_scheduler.LinearLR(optimizer, 1e-2, 1.0, total_iters=5)
+        warmup = optim.lr_scheduler.LinearLR(optimizer, 5*1e-3, 1.0, total_iters=10)
         exp_sch = optim.lr_scheduler.ExponentialLR(optimizer, 0.99)
         scheduler = optim.lr_scheduler.SequentialLR(optimizer, [warmup, exp_sch], [5])
 
@@ -33,8 +35,6 @@ class Trainer:
         print(f'Train size: {len(train_data)}, Validation size: {len(valid_data)}')
 
         for epoch in range(self.epochs):
-            self.model.train()
-            # TODO: early stop
             train_loss = 0
 
             for step, (src_data, tgt_data) in enumerate(train_loader):
@@ -48,18 +48,22 @@ class Trainer:
             scheduler.step()
 
             print(
-                f"Epoch: {epoch + 1}, Train Loss: {train_loss}, Learning rate: {scheduler.get_last_lr()}",
+                f"Epoch: {epoch + 1}; Learning rate: {scheduler.get_last_lr()}; Train - MSE: {train_loss}",
                 end='')
 
-            self.metrics['train_loss'].append(train_loss)
-            self._evaluate(valid_loader, criterion)
+            self.metrics['train']['MSE'].append(train_loss)
+            stop = self._evaluate(valid_loader, criterion, early_stopper)
+            checkpoint(self.model, os.path.join(self.save_path, f'Epoch_{epoch + 1}.pth'))
+            if stop:
+                print(f'Stopped after {epoch + 1} epochs.')
+                break
 
-            checkpoint(self.model, os.path.join(self.save_path, f'Epoch_{epoch}.pth'))
-
-    def _evaluate(self, data_loader, criterion):
+    def _evaluate(self, data_loader, criterion, early_stopper: EarlyStopper):
+        mae = nn.L1Loss()
         self.model.eval()
+        eval_loss = 0
+        mae_loss = 0
         with torch.no_grad():
-            eval_loss = 0
             for src_data, tgt_data in data_loader:
                 ones = torch.ones(tgt_data.shape[0], 1, tgt_data.shape[-1])
                 out = ones
@@ -67,12 +71,12 @@ class Trainer:
                     out = torch.concat((ones, self.model(src_data, out)), dim=1)
                 loss = criterion(out[:, 1:], tgt_data)
                 eval_loss += loss.item() / len(data_loader)
+                mae_loss += mae(out[:, 1:], tgt_data) / len(data_loader)
 
-            print(f", Eval Loss: {eval_loss}")
-        # TODO: MAE, RMSE, MSE for control
+            print(f"; Eval - MSE: {eval_loss}, RMSE: {math.sqrt(eval_loss)}, MAE: {mae_loss}")
 
-        self.metrics['eval_loss'].append(eval_loss)
+            self.metrics['eval']['MSE'].append(eval_loss)
+            self.metrics['eval']['RMSE'].append(math.sqrt(eval_loss))
+            self.metrics['eval']['MAE'].append(mae_loss)
         self.model.train()
-
-    # TODO: static eval function
-    # TODO: save/load model
+        return early_stopper.early_stop(eval_loss)
