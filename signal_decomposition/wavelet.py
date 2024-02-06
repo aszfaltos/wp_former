27ca388ed6b60df3267193ac342dce652d08a5ac
aszfaltos <rnngqd@inf.elte.hu>
2024-02-06 07:00:16 +0100

    Wavelet interpolation and reconstruction

--- signal_decomposition/wavelet.py
+++ signal_decomposition/wavelet.py
@@ -29,8 +29,6 @@ class WaveletWrapper:
     @staticmethod
     def reconstruct(coefs: list[ndarray], wavelet: str, interpolated=True):
         if not interpolated:
-            for coef in coefs:
-                print(coef.shape)
             return pywt.waverec(coefs, wavelet)
 
         reconstructed = []
@@ -38,11 +36,16 @@ class WaveletWrapper:
         div_factor = 1
         for idx, coef in enumerate(reversed(coefs[1:])):
             div_factor = div_factor * 2
-            sampled = coef[np.round((np.arange(math.ceil(coef.shape[0] / div_factor)) / (math.ceil(coef.shape[0] / div_factor) - 1) * (coef.shape[0] - 1))).astype(int)][::1]
-            print(sampled.shape)
+            sampled = coef[
+                          WaveletWrapper._scaled_indexes(math.ceil(coef.shape[0] / div_factor), (coef.shape[0] - 1))
+                      ][::1]
             reconstructed.insert(0, sampled)
 
-        reconstructed.insert(0, coefs[0][np.round((np.arange(math.ceil(coefs[0].shape[0] / div_factor)) / (math.ceil(coefs[0].shape[0] / div_factor) - 1) * (coefs[0].shape[0] - 1))).astype(int)][::1])
+        reconstructed.insert(0,
+                             coefs[0][
+                                 WaveletWrapper._scaled_indexes(math.ceil(coefs[0].shape[0] / div_factor),
+                                                                (coefs[0].shape[0] - 1))
+                             ][::1])
 
         return pywt.waverec(reconstructed, wavelet), reconstructed
 
@@ -52,18 +55,25 @@ class WaveletWrapper:
         if spline == 'akima':
             for coef in coefs:
                 s = scipy.interpolate.Akima1DInterpolator(np.arange(len(coef)), coef)
-                interpolated.append(s((np.arange(original_len) / ((original_len - 1) / (len(coef) - 1))).clip(0, len(coef) - 1)))
+                interpolated.append(s(WaveletWrapper._sample_positions(original_len, len(coef))))
         elif spline == 'cubic':
             for coef in coefs:
                 s = scipy.interpolate.CubicSpline(np.arange(len(coef)), coef)
-                interpolated.append(s((np.arange(original_len) / ((original_len - 1) / (len(coef) - 1))).clip(0, len(coef) - 1)))
+                interpolated.append(s(WaveletWrapper._sample_positions(original_len, len(coef))))
         elif spline == 'linear':
             for coef in coefs:
                 s = scipy.interpolate.interp1d(np.arange(len(coef)), coef)
-                interpolated.append(s((np.arange(original_len) / ((original_len - 1) / (len(coef) - 1))).clip(0, len(coef) - 1)))
+                interpolated.append(s(WaveletWrapper._sample_positions(original_len, len(coef))))
         else:
             return coefs
 
         return interpolated
 
-# TODO: clean up the indexing
+    @staticmethod
+    def _scaled_indexes(num_of_indexes, target_ceil):
+        return np.round(np.arange(num_of_indexes) / (num_of_indexes - 1) * target_ceil).astype(int)
+
+    @staticmethod
+    def _sample_positions(orig_len, coef_len):
+        return ((np.arange(orig_len) / ((orig_len - 1) / (coef_len - 1)))
+                .clip(0, coef_len - 1))
