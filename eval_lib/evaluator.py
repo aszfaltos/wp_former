08428109b580c70deae8a5fbabd0a0d0ed81452f
aszfaltos <rnngqd@inf.elte.hu>
2024-09-10 13:31:29 +0200

    (feat) evaluation architecture: rolling forecasts

--- eval_lib/evaluator.py
+++ eval_lib/evaluator.py
@@ -1,45 +1,111 @@
+import torch
+
+from models import LSTMModel, Transformer
 from .model_loader import load_model
+from trainer_lib.datasets import TimeSeriesWindowedTensorDataset
+import pandas as pd
+from typing import Callable, Tuple
+
 
 class Evaluator:
     def __init__(self):
-        self.models = {}
-        self.evaluations = {}
+        self.models: dict[str, torch.nn.Module] = {}
+        self.forecasts = {}
         self.metrics = {}
 
-    def add_model(self, path, epoch, load_fn, key):
+    def add_model(self, path: str, epoch: int, load_fn: Callable, key: str) -> dict:
         params, model, metrics = load_model(path, epoch, load_fn)
         self.metrics[key] = metrics
         self.models[key] = model
         return params
 
-    def rename_model(self, key, new_key):
+    def rename_model(self, key: str, new_key: str):
         self.models[new_key] = self.models[key]
         del self.models[key]
         self.metrics[new_key] = self.metrics[key]
         del self.metrics[key]
-        if key in self.evaluations.keys():
-            self.evaluations[new_key] = self.evaluations[key]
-            del self.evaluations[key]
+        if key in self.forecasts.keys():
+            self.forecasts[new_key] = self.forecasts[key]
+            del self.forecasts[key]
 
-    def remove_model(self, key):
+    def remove_model(self, key: str):
         del self.models[key]
         del self.metrics[key]
-        self.evaluations.pop(key, None)
+        self.forecasts.pop(key, None)
 
-    def list_models(self):
-        return self.models.keys()
+    def list_models(self) -> list[str]:
+        return list(self.models.keys())
 
-    def eval_model_on_dataset(self, key, dataset):
-        pass
+    def generate_rolling_forecast(self, key: str, dataset: TimeSeriesWindowedTensorDataset, pred_len: int) -> Tuple[list, list]:
+        model = self.models[key]
 
-    def eval_models_on_dataset(self, dataset):
-        pass
+        gt, f = [], []
 
-    def generate_rolling_forecast(self, key, dataset):
-        pass
+        if isinstance(model, LSTMModel):
+            gt, f = self._lstm_forecast(model, dataset, pred_len)
+        elif isinstance(model, Transformer):
+            gt, f = self._transformer_forecast(model, dataset, pred_len)
 
-    def generate_rolling_forecasts(self, dataset):
-        pass
+        return gt, f
+
+
+    def generate_rolling_forecasts(self, dataset: TimeSeriesWindowedTensorDataset, pred_len):
+        forecasts = []
+
+        for key in self.list_models():
+            forecasts.append(self.generate_rolling_forecast(key, dataset, pred_len))
+
+        return forecasts
 
     def generate_evaluation_table(self):
-        pass
+        def bold_min(col):
+            bold = 'font-weight: bold'
+            default = ''
+
+            min_in_col = col.min()
+            return [bold if v == min_in_col else default for v in col]
+
+        d = {'mse': [self.metrics[key]['test']['MSE'][-1] for key in self.list_models()],
+             'rmse': [self.metrics[key]['test']['RMSE'][-1] for key in self.list_models()],
+             'mae': [self.metrics[key]['test']['MAE'][-1] for key in self.list_models()]}
+        df = pd.DataFrame(data=d, index=[key for key in self.list_models()])
+        df.style.apply(bold_min, axis=0)
+
+    @staticmethod
+    def _lstm_forecast(model: LSTMModel, dataset: TimeSeriesWindowedTensorDataset, pred_len: int):
+        model.eval()
+        with torch.no_grad():
+            gt = []
+            p = []
+            for shift_offset in range(0, len(dataset), pred_len * dataset.ws_y):
+                src, tgt = dataset[shift_offset]
+                src.unsqueeze(0)
+                for i in range(pred_len):
+                    out = model(src)
+                    in_data = torch.concatenate((dataset[shift_offset][0].unsqueeze(0), out.unsqueeze(-2)), dim=1)
+
+                p.append(dataset.get_sequence_from_y_windows(in_data[:, -pred_len:, :].detach()).to('cpu'))
+                gt.append(dataset.get_sequence_from_y_windows(tgt).to('cpu'))
+
+        return gt, p
+
+
+    @staticmethod
+    def _transformer_forecast(model: Transformer, dataset: TimeSeriesWindowedTensorDataset, pred_len: int):
+        model.eval()
+        start_token = torch.ones(1, 1, dataset[0][1].shape[-1])
+
+        with torch.no_grad():
+            out = start_token
+            gt = []
+            p = []
+            for shift_offset in range(0, len(dataset), pred_len * dataset.ws_y):
+                src, tgt = dataset[shift_offset]
+                src = src.unsqueeze(0)
+                for i in range(pred_len):
+                    out = torch.concatenate((start_token, model(src, out)), dim=1)
+
+                p.append(dataset.get_sequence_from_y_windows(out[:, 1:, :].detach()).to('cpu'))
+                gt.append(dataset.get_sequence_from_y_windows(tgt).to('cpu'))
+
+        return gt, p
--- eval_lib/model_loader.py
+++ eval_lib/model_loader.py
@@ -1,67 +1,18 @@
 import os
 import json
+from typing import Callable
+
 from torch import nn
 from models import Transformer, TransformerParams, VPTransformer, VPTransformerParams, LSTMParams, LSTMModel
 from trainer_lib.utils import resume
 
 
-
-def model_from_params(params: dict, model_type) -> nn.Module:
-    params['kind'] = 'lstm'
-    print(params['kind'])
-    if params['kind'] == 'transformer':
-        transformer_params = TransformerParams(
-            src_size=params['src_size'] * params['src_window'],
-            tgt_size=params['tgt_size'] * params['tgt_window'],
-            d_model=params['d_model'],
-            num_heads=params['num_heads'],
-            num_layers=params['num_layers'],
-            d_ff=params['d_ff'] * params['d_model'],
-            max_seq_length=max(params['src_seq_length'], params['tgt_seq_length']),
-            dropout=params['dropout']
-        )
-
-        model = model_type(transformer_params)
-        return model
-    elif params['kind'] == 'vp_transformer':
-        transformer_params = VPTransformerParams(
-            vp_bases=params['vp_bases'],
-            vp_penalty=params['vp_penalty'],
-            src_size=params['src_size'] * params['src_window'],
-            tgt_size=params['tgt_size'] * params['tgt_window'],
-            d_model=params['d_model'],
-            num_heads=params['num_heads'],
-            num_layers=params['num_layers'],
-            d_ff=params['d_ff'] * params['d_model'],
-            max_seq_length=max(params['src_seq_length'], params['tgt_seq_length']),
-            dropout=params['dropout']
-        )
-
-        model = model_type(transformer_params)
-        return model
-    elif params['kind'] == 'lstm':
-        lstm_params = LSTMParams(
-            in_features=params['in_features'],
-            hidden_size=params['hidden_size'],
-            num_layers=params['num_layers'],
-            out_features=params['out_features'],
-            dropout=params['dropout'],
-            in_noise=params['in_noise'],
-            hid_noise=params['hid_noise'],
-            bidirectional=params['bidirectional']
-        )
-
-        model = model_type(lstm_params)
-        return model
-
-
-def load_model(path: str, epoch: int, load_fn):
+def load_model(path: str, epoch: int, load_fn: Callable):
     with open(os.path.join(path, 'params.json'), 'r') as fp:
         params = json.load(fp)
 
-    model = load_lstm(params)
+    model = load_fn(params)
     resume(model, os.path.join(path, f'{epoch}.pth'))
-    model.eval()
 
     with open(os.path.join(path, f'{epoch}.json'), 'r') as fp:
         metrics = json.load(fp)
