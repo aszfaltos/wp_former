1675617888019a02995dac52e13aa1f49a13d69f
aszfaltos <rnngqd@inf.elte.hu>
2024-09-24 08:15:29 +0200

    (feat) training functions for vp and wavelet variants

--- training.py
+++ training.py
@@ -16,21 +16,7 @@ def load_data(sample_size, start_idx):
     return utils.sample(df, sample_size, start_idx=start_idx)
 
 
-def set_default_options():
-    params = {
-        'kind': ['transformer'],
-        'd_model': [128],
-        'num_heads': [2],
-        'num_layers': [1],
-        'd_ff': [2],
-        'src_seq_length': [24],
-        'tgt_seq_length': [1],
-        'src_window': [8],
-        'tgt_window': [1],
-        'dropout': [0.1],
-    }
-    grid = Grid(params)
-
+def train_lstm(training_data, logger):
     training_opts = TrainerOptions(
         batch_size=1024,
         epochs=600,
@@ -44,92 +30,42 @@ def set_default_options():
         save_path=''
     )
 
-    return grid, training_opts
-
-
-def train_regular_transformer(training_data):
-    grid, training_opts = set_default_options()
     grid_search_opts = GridSearchOptions(
-        root_save_path='./trained/regular/',
+        root_save_path='./trained/lstm/',
+        run_nums=5,
         valid_split=0.2,
         test_split=0.2,
-        window_step_size=4,
-        random_seed=50,
-        use_start_token=True,
-        preprocess_y=False
+        random_seed=42,
     )
 
-    transformer_grid_search(grid, training_data, training_opts, grid_search_opts)
-
-
-def train_eemd_transformer(training_data):
-    grid, training_opts = set_default_options()
-
-    grid_search_opts = GridSearchOptions(
-        root_save_path='./trained/eemd/',
-        valid_split=0.2,
-        test_split=0.2,
-        window_step_size=4,
-        random_seed=46,
-        use_start_token=True,
-        preprocess_y=False
+    dataset_config = TimeSeriesWindowedDatasetConfig(
+        src_window_size=1,
+        tgt_window_size=1,
+        src_sequence_length=24,
+        tgt_sequence_length=1,
+        step_size=4,
+        add_start_token=False,
+        preprocess_y=True
     )
-
-    transformer_grid_search(grid, training_data, training_opts, grid_search_opts,
-                            preprocessor=eemd.EEMDPreprocessor(imfs=3, trials=20))
-
-
-def train_wavelet_transformer(training_data):
-    grid, training_opts = set_default_options()
-
-    grid_search_opts = GridSearchOptions(
-        root_save_path='./trained/wavelet/',
-        valid_split=0.2,
-        test_split=0.2,
-        window_step_size=4,
-        random_seed=50,
-        use_start_token=True,
-        preprocess_y=False
-    )
-
-    transformer_grid_search(grid, training_data, training_opts, grid_search_opts,
-                            preprocessor=wavelet.WaveletPreprocessor('db2'))
-
-
-def train_wavelet_vp_transformer(training_data):
-    _, training_opts = set_default_options()
+    dataset = TimeSeriesWindowedTensorDataset(training_data, dataset_config)
 
     params = {
-        'kind': ['vp_transformer'],
-        'vp_bases': [8],
-        'vp_penalty': [0.2],
-        'd_model': [128],
-        'num_heads': [2],
-        'num_layers': [1],
-        'd_ff': [2],
-        'src_seq_length': [24*8],
-        'tgt_seq_length': [1],
-        'src_window': [1],
-        'tgt_window': [1],
-        'dropout': [0.1],
+        'in_features': [3],
+        'hidden_size': [64],
+        'num_layers': [2],
+        'out_features': [15],
+        'dropout': [.03],
+        'in_noise': [.0],
+        'hid_noise': [.01],
+        'bidirectional': [True],
     }
     grid = Grid(params)
 
-    grid_search_opts = GridSearchOptions(
-        root_save_path='./trained/vp_wavelet/',
-        valid_split=0.2,
-        test_split=0.2,
-        window_step_size=4,
-        random_seed=50,
-        use_start_token=True,
-        preprocess_y=False
-    )
-
-    transformer_grid_search(grid, training_data, training_opts, grid_search_opts,
-                            preprocessor=wavelet.WaveletPreprocessor('db2'))
+    grid_search = LSTMGridSearch(dataset, training_opts, grid_search_opts, logger)
+    grid_search.search(grid)
 
 
-def train_lstm(training_data, logger):
+def train_lstm_wavelet(training_data, logger):
     training_opts = TrainerOptions(
         batch_size=1024,
         epochs=600,
@@ -178,7 +114,105 @@ def train_lstm(training_data, logger):
     grid_search.search(grid)
 
 
+def train_vp_lstm(training_data, logger):
+    training_opts = TrainerOptions(
+        batch_size=1024,
+        epochs=600,
+        learning_rate=1e-3,
+        learning_rate_decay=.999,
+        weight_decay=1e-5,
+        gradient_accumulation_steps=2,
+        early_stopping_patience=30,
+        early_stopping_min_delta=0.01,
+        save_every_n_epochs=5,
+        save_path=''
+    )
+
+    grid_search_opts = GridSearchOptions(
+        root_save_path='./trained/vp_lstm/',
+        run_nums=5,
+        valid_split=0.2,
+        test_split=0.2,
+        random_seed=42,
+    )
+
+    dataset_config = TimeSeriesWindowedDatasetConfig(
+        src_window_size=1,
+        tgt_window_size=1,
+        src_sequence_length=24,
+        tgt_sequence_length=1,
+        step_size=4,
+        add_start_token=False,
+        preprocess_y=True
+    )
+    dataset = TimeSeriesWindowedTensorDataset(training_data, dataset_config)
+
+    params = {
+        'in_features': [3],
+        'hidden_size': [64],
+        'num_layers': [2],
+        'out_features': [15],
+        'dropout': [.03],
+        'in_noise': [.0],
+        'hid_noise': [.01],
+        'bidirectional': [True],
+    }
+    grid = Grid(params)
+
+    grid_search = LSTMGridSearch(dataset, training_opts, grid_search_opts, logger)
+    grid_search.search(grid)
+
+
 def train_transformer(training_data, logger):
+    training_opts = TrainerOptions(
+        batch_size=1024,
+        epochs=600,
+        learning_rate=1e-3,
+        learning_rate_decay=.999,
+        weight_decay=1e-5,
+        gradient_accumulation_steps=2,
+        early_stopping_patience=30,
+        early_stopping_min_delta=0.01,
+        save_every_n_epochs=5,
+        save_path=''
+    )
+
+    grid_search_opts = GridSearchOptions(
+        root_save_path='./trained/transformer/',
+        run_nums=5,
+        valid_split=0.2,
+        test_split=0.2,
+        random_seed=42,
+    )
+
+    dataset_config = TimeSeriesWindowedDatasetConfig(
+        src_window_size=1,
+        tgt_window_size=1,
+        src_sequence_length=24,
+        tgt_sequence_length=1,
+        step_size=4,
+        add_start_token=True,
+        preprocess_y=True
+    )
+    dataset = TimeSeriesWindowedTensorDataset(training_data, dataset_config)
+
+    params = {
+        "src_size": [3],
+        "tgt_size": [3],
+        "d_model": [64],
+        "num_heads": [2, 4],
+        "num_layers": [2, 4],
+        "d_ff": [128, 256],
+        "max_seq_length": [24],
+        "dropout": [.01, .03, .1]
+    }
+    grid = Grid(params)
+
+    grid_search = TransformerGridSearch(dataset, training_opts, grid_search_opts, logger)
+    grid_search.search(grid)
+
+
+def train_transformer_wavelet(training_data, logger):
     training_opts = TrainerOptions(
         batch_size=1024,
         epochs=600,
@@ -220,6 +254,57 @@ def train_transformer(training_data, logger):
         "d_ff": [128, 256],
         "max_seq_length": [24],
         "dropout": [.01, .03, .1]
+    }
+    grid = Grid(params)
+
+    grid_search = TransformerGridSearch(dataset, training_opts, grid_search_opts, logger)
+    grid_search.search(grid)
+
+
+def train_vp_transformer(training_data, logger):
+    training_opts = TrainerOptions(
+        batch_size=1024,
+        epochs=600,
+        learning_rate=1e-3,
+        learning_rate_decay=.999,
+        weight_decay=1e-5,
+        gradient_accumulation_steps=2,
+        early_stopping_patience=30,
+        early_stopping_min_delta=0.01,
+        save_every_n_epochs=5,
+        save_path=''
+    )
+
+    grid_search_opts = GridSearchOptions(
+        root_save_path='./trained/vp_transformer/',
+        run_nums=5,
+        valid_split=0.2,
+        test_split=0.2,
+        random_seed=42,
+    )
+
+    dataset_config = TimeSeriesWindowedDatasetConfig(
+        src_window_size=1,
+        tgt_window_size=1,
+        src_sequence_length=24,
+        tgt_sequence_length=1,
+        step_size=4,
+        add_start_token=True,
+        preprocess_y=True
+    )
+    dataset = TimeSeriesWindowedTensorDataset(training_data, dataset_config)
+
+    params = {
+        "src_size": [3],
+        "tgt_size": [3],
+        "d_model": [64],
+        "num_heads": [2, 4],
+        "num_layers": [2, 4],
+        "d_ff": [128, 256],
+        "max_seq_length": [24],
+        "dropout": [.01, .03, .1],
+        'vp_bases': [8],
+        'vp_penalty': [0.2],
     }
     grid = Grid(params)
 
