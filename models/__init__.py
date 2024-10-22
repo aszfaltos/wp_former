81f8218ce43a0f21003e97b5cc02f5d1bdf70f0a
aszfaltos <rnngqd@inf.elte.hu>
2024-10-22 08:44:01 +0200

    (feat) include spacetimeformer model

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ['Transformer', 'TransformerParams', 'VPTransformer', 'VPTransformerParams', 'LSTMModel', 'LSTMParams', 'VPLSTMParams', 'VPLSTMModel']
+__all__ = ['Transformer', 'TransformerParams', 'VPTransformer', 'VPTransformerParams', 'LSTMModel', 'LSTMParams', 'VPLSTMParams', 'VPLSTMModel', 'Spacetimeformer_Forecaster']
 from .transformer import Transformer, TransformerParams
 from .vp_transformer import VPTransformer, VPTransformerParams
 from .lstm import LSTMModel, LSTMParams
 from .vp_lstm import VPLSTMModel, VPLSTMParams
+from .spacetimeformer import Spacetimeformer_Forecaster
